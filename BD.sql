296634c2af4ae31b9d1a48b4be953cbcfd0f3fc0
João Kikuti <joaovitorkikuti@gmail.com>
2024-09-01 20:59:23 -0300

    atualização banco
    feito pente fino do banco

--- BD.sql
+++ BD.sql
@@ -12,50 +12,50 @@ CREATE SCHEMA IF NOT EXISTS loja;
 
 -- Criar a tabela pedido
 CREATE TABLE loja.pedido (
-    id INT SERIAL,
+    id SERIAL,
     valor_total DECIMAL(7,2),
     data_pedido TIMESTAMP,
 
-    CONSTRAINT pk_pedido PRIMARY KEY(id),
+    CONSTRAINT pk_pedido PRIMARY KEY(id)
 );
 
 -- Criar a tabela cliente
 CREATE TABLE loja.cliente (
-    id INT SERIAL,
+    id SERIAL,
     cpf CHAR(14) NOT NULL,
     nome VARCHAR(200),
     email VARCHAR(200) NOT NULL,
 
     id_pedido INT,
 
-    CONSTRAINT pk_cliente PRIMARY KEY(id_cliente),
+    CONSTRAINT pk_cliente PRIMARY KEY(id),
     CONSTRAINT uk_cliente_cpf UNIQUE(cpf),
     CONSTRAINT uk_cliente_email UNIQUE(email),
 
-    CONSTRAINT fk_pedido_cliente FOREIGN KEY(id_pedido) REFERENCES pedido(id),
+    CONSTRAINT fk_pedido_cliente FOREIGN KEY(id_pedido) REFERENCES loja.pedido(id)
 );
 
 -- Criar a tabela endereco
-CREATE TABLE loja.endereco {
-    id INT SERIAL,
+CREATE TABLE loja.endereco (
+    id SERIAL,
     end_num INT,
     estado CHAR(2),
     cidade VARCHAR(200),
     bairro VARCHAR(200),
     rua VARCHAR(200),
-    cep INT(8),
+    cep INT,
 
     id_cliente INT,
 
     CONSTRAINT pk_endereco PRIMARY KEY(id),
     CONSTRAINT uk_endereco_cliente UNIQUE(end_num, cep),
 
-    CONSTRAINT fk_cliente_endereco FOREIGN KEY(id_cliente) REFERENCES cliente(id),
-};
+    CONSTRAINT fk_cliente_endereco FOREIGN KEY(id_cliente) REFERENCES loja.cliente(id)
+);
 
 -- Criar a tabela action_figure
 CREATE TABLE loja.action_figure (
-    id INT SERIAL,
+    id SERIAL,
     personagem VARCHAR(200),
     universo VARCHAR(200),
     fabricante VARCHAR(200),
@@ -65,7 +65,7 @@ CREATE TABLE loja.action_figure (
     categoria VARCHAR(200),
     estoque INT,
 
-    CONSTRAINT pk_action_figure PRIMARY KEY(id),
+    CONSTRAINT pk_action_figure PRIMARY KEY(id)
 );
 
 -- Criar a tabela pedido_tem_action_figure
@@ -76,13 +76,13 @@ CREATE TABLE loja.pedido_tem_action_figure (
 
     CONSTRAINT pk_pedido_tem_action_figure PRIMARY KEY(id_pedido, id_action_figure),
 
-    CONSTRAINT fk_id_pedido FOREIGN KEY(id_pedido) REFERENCES pedido(id),
-    CONSTRAINT fk_id_action_figure FOREIGN KEY(id_action_figure) REFERENCES action_figure(id),
+    CONSTRAINT fk_id_pedido FOREIGN KEY(id_pedido) REFERENCES loja.pedido(id),
+    CONSTRAINT fk_id_action_figure FOREIGN KEY(id_action_figure) REFERENCES loja.action_figure(id)
 );
 
 -- Criar a tabela material
 CREATE TABLE loja.material (
-    id INT SERIAL,
+    id SERIAL,
     tipo_material VARCHAR(200),
 
     CONSTRAINT pk_material PRIMARY KEY(id)
@@ -96,19 +96,19 @@ CREATE TABLE loja.action_figure_material (
     CONSTRAINT pk_action_figure_material PRIMARY KEY(id_action_figure, id_material),
 
     CONSTRAINT fk_id_action_figure FOREIGN KEY(id_action_figure) REFERENCES loja.action_figure(id),
-    CONSTRAINT fk_id_material FOREIGN KEY(id_material) REFERENCES loja.material(id),
+    CONSTRAINT fk_id_material FOREIGN KEY(id_material) REFERENCES loja.material(id)
 );
 
 -- Criar a tabela imagem
 CREATE TABLE loja.imagem (
-    id INT SERIAL,
+    id SERIAL,
     imagem_url VARCHAR(200),
 
     id_action_figure INT,
 
     CONSTRAINT pk_imagem PRIMARY KEY(id),
 
-    CONSTRAINT fk_id_action_figure FOREIGN KEY(id_action_figure) REFERENCES loja.action_figure(id),
+    CONSTRAINT fk_id_action_figure FOREIGN KEY(id_action_figure) REFERENCES loja.action_figure(id)
 );
 
 --------------
